b6196e97a8b906232a690af02035c94ba5bae05f
charliehora <charliehora@gmail.com>
2023-03-20 13:49:10 -0500

    start page

---
+++ app/src/main/java/com/example/footballquiz/startPage.kt
@@ -0,0 +1,38 @@
+package com.example.footballquiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import com.example.footballquiz.databinding.StartPageBinding
+
+    class startPage : AppCompatActivity() {
+
+        private lateinit var startButton: Button
+
+
+        //private lateinit var binding: StartPageBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.start_page)
+
+            startButton = findViewById(R.id.begin_the_quiz)
+            /*binding = StartPageBinding.inflate(layoutInflater)
+            setContentView(binding.root)*/
+
+            /*binding.startButton.setOnClickListener { view: View ->
+                val intent = Intent(this, MainActivity::class.java)
+            }*/
+
+            startButton.setOnClickListener { view: View ->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+
+
